c07332cd78ddeaab31f3351f0eea08b05a801b60
Musa Sayar <musasayar67@gmail.com>
2023-12-29 16:38:05 +0300

    edit css

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,18 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
 import { sendFormMail } from "../redux/portfolioSlice.jsx";
+import { MdOutgoingMail } from "react-icons/md";
 const Form = () => {
   const { sendFormModal } = useSelector((state) => state.portfolio);
 
   const dispatch = useDispatch();
   return (
     <button
-      className=" fixed bottom-4 right-4 px-4 py-2 bg-gray-400 text-white font-semibold drop-shadow-xl
-        animate-bounce mt-4 rounded-md hover:bg-gray-500 duration-150 ease-in-out"
+      className=" fixed bottom-4 right-4 px-4 py-4 bg-gray-500 text-white font-semibold  shadow-2xl drop-shadow-xl
+        animate-bounce mt-4 rounded-full hover:bg-gray-600 duration-150 ease-in-out"
       onClick={() => {
         dispatch(sendFormMail(true));
       }}
     >
-      Send Mail
+      <MdOutgoingMail size={26} />
     </button>
   );
 };
--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -2,17 +2,15 @@ import React from 'react'
 import { ColorRing } from 'react-loader-spinner';
 const Loading = () => {
   return (
-    <div className='flex items-center justify-center h-[100vh]'>
+    <div className="flex items-center justify-center h-[100vh]">
       <ColorRing
         visible={true}
         height="150"
         width="150"
         ariaLabel="color-ring-loading"
-      
         wrapperClass="color-ring-wrapper"
-        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#007bff"]}
+        colors={["#64748b", "#475569", "#334155", "#1e293b", "#007bff"]}
       />
-      
     </div>
   );
 }
