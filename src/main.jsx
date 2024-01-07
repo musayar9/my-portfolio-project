1d2481fa050dd987d5ac117e6c3dd8f62ffa0e3a
Musa Sayar <musasayar67@gmail.com>
2024-01-07 12:49:50 +0300

    remove strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import store from "./redux/store.jsx";
 import 'react-toastify/dist/ReactToastify.css';
 import {BrowserRouter} from "react-router-dom"
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+
 );
